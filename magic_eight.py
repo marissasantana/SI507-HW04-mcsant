e3911bcdce9f5edad1101718bdf65882f094a995
Ivie Okieimen <valms_94@0587388659.wireless.umich.net>
2018-01-29 14:04:16 -0500

    updating file

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,10 @@
-def Magic(self):
+from random import randrange
+import random
+
+def Magic():
     question = input("What is your question?")
-    return question 
+    random_words = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you know", "Cannot predict now", "concentrate and ask again"]
+    magic_random = randrange(0, len(random_words))
+    print(random_words[magic_random])
+
+Magic()
